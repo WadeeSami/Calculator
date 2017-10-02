47286d03b5c137b91d746a99ad31ebb5665f6729
Wadee Sami <wadee@medclue.com>
2017-10-02 21:33:59 +0300

    Initial Commit

---
+++ CalculatorV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculatorV2
+//
+//  Created by Wadee Sami on 10/2/17.
+//  Copyright © 2017 Wadee AbuZant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
